4f5a246014e68f8fa87473466b5d22e1eff47a0c
Alejandro Mezcua <alejandro.mezcua@byteabyte.net>
2016-09-26 12:38:26 +0200

    Reformat code to eliminate unused variables warnings.

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Jwt.Mixfile do
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(:dev), do: ["lib", "test/support"]
+  defp elixirc_paths(:dev),  do: ["lib", "test/support"]
   defp elixirc_paths(_),     do: ["lib"]
 
   defp deps do
