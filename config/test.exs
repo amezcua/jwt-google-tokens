387ceee662ed3d76c13f664a104d3fb9130fb9f6
Alejandro Mezcua <alejandro.mezcua@byteabyte.net>
2017-01-11 18:17:44 +0100

    Added support for verifying the tokens returned by the Firebase JavaScript authentication API.
    The certificates location and format is different from those returned by the Android API and now both URLs are retrieved and cached in search for the public key certificates associated to the tokens.

--- config/test.exs
+++ config/test.exs
@@ -3,4 +3,5 @@ use Mix.Config
 config :logger, level: :debug
 
 config :jwt, :googlecerts, Jwt.GoogleCerts.PublicKey.Mock
+config :jwt, :firebasecerts, Jwt.FirebaseCerts.PublicKey.Mock
 config :jwt, :timeutils, Jwt.TimeUtils.Mock
---
+++ lib/firebasecerts.ex
@@ -0,0 +1,28 @@
+defmodule Jwt.FirebaseCerts.PublicKey do
+
+  @httpclient Jwt.HttpCacheClient
+  @certs_url "https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com"
+
+  def getfor(id), do: fetch id
+
+  defp fetch(id) do
+    @httpclient.get!(@certs_url)
+        |> get_response_body
+        |> extract_public_key_for_id(id)
+  end
+
+  defp get_response_body(%{body: body, headers: _headers, status_code: 200}), do: {:ok, body}
+  defp get_response_body(%{body: body, headers: _headers, status_code: _status_code}), do: {:error, body}
+
+  defp extract_public_key_for_id({:error, _}, _id), do: nil
+  defp extract_public_key_for_id({:ok, body}, id) do
+    {:ok, parsed} = Poison.Parser.parse body
+
+    cert = parsed[id]
+
+    case cert do
+      nil -> {:error, "Public key id not found"}
+      cert_string -> Jwt.PemParser.extract_exponent_and_modulus_from_pem_cert cert_string
+    end
+  end
+end
