16c61fa2a5295db85ce35cebae6e2cbbaed9d5b9
Arjan Scherpenisse <arjan@scherpenisse.net>
2019-03-26 16:52:14 +0100

    Run mix format, support cowboy 2.x

--- mix.exs
+++ mix.exs
@@ -2,35 +2,37 @@ defmodule Jwt.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :jwt,
-     version: "0.6.0",
-     elixir: "~> 1.3",
-     elixirc_paths: elixirc_paths(Mix.env),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :jwt,
+      version: "0.6.0",
+      elixir: "~> 1.3",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   def application do
     [
-        applications: [:logger, :httpoison, :cowboy, :plug, :timex],
-        mod: {Jwt.Cache.Ets, []}
+      applications: [:logger, :httpoison, :cowboy, :plug, :timex],
+      mod: {Jwt.Cache.Ets, []}
     ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(:dev),  do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(:dev), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
-    {:httpoison, "~> 1.0" },
-    {:poison, "~> 2.0 or ~> 3.0" },
-    {:ex_doc, only: :dev, runtime: false, github: "elixir-lang/ex_doc" },
-    {:cowboy, "~> 1.0"},
-    {:plug, "~> 1.0"},
-    {:stash, "~> 1.0"},
-    {:timex, "~> 3.0"}
+      {:httpoison, "~> 1.0"},
+      {:poison, "~> 2.0 or ~> 3.0"},
+      {:cowboy, "~> 1.0 or ~> 2.0"},
+      {:plug, "~> 1.0"},
+      {:stash, "~> 1.0"},
+      {:timex, "~> 3.0"},
+      {:ex_doc, only: :dev, runtime: false, github: "elixir-lang/ex_doc"}
     ]
   end
 end
